fc1950893241adcec734f356905b765e42cce35f
BrandonCoats <bgcoats1313@gmail.com>
2017-08-07 09:51:16 -0600

    bot creds

--- Feature/Feature/Program.cs
+++ Feature/Feature/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
 
 namespace Feature
 {
@@ -10,6 +12,15 @@ namespace Feature
     {
         static void Main(string[] args)
         {
+            new Program().Start();
+        }
+        private IDiscordClient client;
+        public void Start()
+        {
+            client = new IDiscordClient(x =>
+            {
+
+            });
         }
     }
 }
