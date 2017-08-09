674e0b8e076cbff11cf77f34b369b69a75c0aa92
BrandonCoats <bgcoats1313@gmail.com>
2017-08-09 09:59:29 -0600

    random image stuff

--- DiscordFeature/DiscordFeature/MyBot.cs
+++ DiscordFeature/DiscordFeature/MyBot.cs
@@ -12,6 +12,7 @@ namespace DiscordFeature
     public class MyBot
     {
         DiscordClient discord;
+        CommandService commands;
         BotProcessor bp = new BotProcessor();
         public MyBot()
         {
@@ -25,19 +26,58 @@ namespace DiscordFeature
                 x.PrefixChar = 'J';
                 x.AllowMentionPrefix = true;
             });
-            var commands = discord.GetService<CommandService>();
+           commands = discord.GetService<CommandService>();
             commands.CreateCommand("Slut")
                 .Do(async (e) =>
                 {
                    await e.Channel.SendMessage("Suck it fagot");
                 });
+            RegisterImages();
             discord.ExecuteAndWait(async () =>
             {
                 await discord.Connect("MzQ0MTQzOTEyNDEzNDI5NzYx.DGtyIA.y_wBcXzuLsyMEk7utz5awPyz41Y", TokenType.Bot);
             });
             
         }
+        private void RegisterImages()
+        {
+            String random = RandomImage();
+             
+            commands.CreateCommand("dank").Do(async (e) =>
+            {
+                await e.Channel.SendFile(random);
+            });
+        }
+        private String RandomImage()
+        {
+            String path = "";
+            Random rand = new Random();
+            string currentDir = Environment.CurrentDirectory;
+            int randNum = rand.Next(0,5);
+            switch (randNum)
+            {
+                case 0:
+                    path =currentDir+"\\Memes\\f60b0f08fb9fac8319e275e31fc7da55bd021ec2a6e343edf9ffcf7642934020_1.jpg";
+                    break; 
+                case 1:
+                    path = currentDir +"\\Memes\\image(1).jpg";
+                    break;
+                case 2:
+                    path = currentDir + "\\Memes\\image(2).jpg";
+                    break;
+                case 3:
+                    path = currentDir + "\\Memes\\image.jpg";
+                    break;
+                case 4:
+                    path = currentDir + "\\Memes\\image.png";
+                    break;
+                default:
+                    Console.WriteLine("Broke asf");
+                    break;
+            }
 
+            return path;
+        }
 
         private void Log(object sender,LogMessageEventArgs e)
         {
