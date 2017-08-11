d0f7232040d00d86d72b7cfc095102423c2232fe
BrandonCoats <bgcoats1313@gmail.com>
2017-08-11 08:01:35 -0600

    heyy stuff

--- DiscordFeature/DiscordFeature/MyBot.cs
+++ DiscordFeature/DiscordFeature/MyBot.cs
@@ -1,5 +1,6 @@
 ﻿using Discord;
 using Discord.Commands;
+using Discord.Audio;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,16 +14,23 @@ namespace DiscordFeature
     {
         DiscordClient discord;
         CommandService commands;
+        List<string> helpList;
         BotProcessor bp = new BotProcessor();
         List<string> danks;
         public MyBot()
         {
             danks = new List<string>();
+            helpList = new List<string>();
             danks.Add("\\Memes\\f60b0f08fb9fac8319e275e31fc7da55bd021ec2a6e343edf9ffcf7642934020_1.jpg");
             danks.Add("\\Memes\\image1.jpg");
             danks.Add("\\Memes\\image2.jpg");
             danks.Add("\\Memes\\image.jpg");
             danks.Add("\\Memes\\image.png");
+            helpList.Add("echo");
+           // helpList.Add("talk");
+            helpList.Add("Slut");
+            helpList.Add("purge");
+            helpList.Add("dank");
             discord = new DiscordClient(x =>
             {
                 x.LogLevel = LogSeverity.Info;
@@ -42,6 +50,8 @@ namespace DiscordFeature
             RegisterImages();
             RegisterPurgeCommand();
             RegisterEcho();
+            RegisterHelp();
+            RegisterHey();
             discord.ExecuteAndWait(async () =>
             {
                 await discord.Connect("MzQ0MTQzOTEyNDEzNDI5NzYx.DGtyIA.y_wBcXzuLsyMEk7utz5awPyz41Y", TokenType.Bot);
@@ -87,8 +97,40 @@ namespace DiscordFeature
                 {
                     fullResponse = fullResponse +" "+ e.GetArg(i);
                 }
+                Message[] messages;
+                messages = await e.Channel.DownloadMessages(1);
+                await e.Channel.DeleteMessages(messages);
                 await e.Channel.SendMessage(fullResponse);
            
+            });
+        }
+        private void RegisterHelp()
+        {
+            commands.CreateCommand("help").Parameter("phrase", ParameterType.Multiple).Do(async (e) =>
+            {
+               
+                String fullResponse = "";
+                for (int i = 0; i < helpList.Count; i++)
+                {
+                    fullResponse = fullResponse + " " + helpList.ElementAt(i);
+                }
+                await e.Channel.SendTTSMessage(fullResponse);
+                await e.Channel.SendMessage(fullResponse);
+
+            });
+        }
+        private void RegisterHey()
+        {
+            commands.CreateCommand("hey").Parameter("phrase", ParameterType.Multiple).Do(async (e) =>
+            {
+                string currentDir = Environment.CurrentDirectory;
+                string dankPost = "\\SoundMemes\\heyyyy.mp3";
+                currentDir = currentDir.Replace("\\bin\\Debug", dankPost);
+                Console.WriteLine("In hey");
+                await e.Channel.JoinAudio();
+                ///await e.Channel.SendMessage(currentDir);
+                
+
             });
         }
         private void Log(object sender,LogMessageEventArgs e)
